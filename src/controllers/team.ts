017ef29c655884dab2956d0454603bf3dcc9a033
seun jay <seunjay92@gmail.com>
2019-09-17 20:05:52 +0100

    create controller to create team

--- src/controllers/team.ts
+++ src/controllers/team.ts
@@ -2,11 +2,48 @@ import { validateTeam } from '../validators/validateTeam';
 import { Request, Response } from 'express';
 import { Team } from '../models/Team';
 
-export const viewteam = async (_req: Request, res: Response) => {
+export const viewTeam = async (_req: Request, res: Response) => {
   try {
     const teams = await Team.find({ isDeleted: false });
-    res.status(200).json({ success: true, data: teams });
+    return res.status(200).json({ success: true, data: teams });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export const createTeam = async (req: Request, res: Response) => {
+  const { error } = validateTeam(req.body);
+  if (error) return res.status(401).send({ error: error.details[0].message });
+
+  try {
+    const {
+      name,
+      email,
+      coach,
+      country,
+      founded,
+      stadium_name,
+      stadium_capacity,
+    } = req.body;
+
+    const teamCheck = await Team.findOne({ email });
+    if (teamCheck)
+      return res.status(400).json({
+        success: false,
+        message: 'A user with this team already exists!',
+      });
+
+    const newTeam = await new Team({
+      name,
+      email,
+      coach,
+      country,
+      founded,
+      stadium_name,
+      stadium_capacity,
+    }).save();
+    return res.status(200).json({ success: true, data: newTeam });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
   }
 };
