b7d905bb99f60cedb09e8f6c007aa0b7542a90ba
seun jay <seunjay92@gmail.com>
2019-09-17 15:37:06 +0100

    create middleware to authenticate a user

---
+++ src/middleware/auth.ts
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { User } from '../models/User';
+
+//@ts-ignore
+async function auth(req: Request, res: Response, next: NextFunction) {
+  let token;
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+      return res.status(401).send({
+        data: {
+          message: 'You are not logged in! Please log in to get access.',
+        },
+      });
+    }
+
+    const decoded: any = jwt.verify(token, <any>process.env.JWT_PRIVATE_KEY);
+    const currentUser = await User.findById(decoded._id);
+
+    if (!currentUser) {
+      return res.status(401).send({
+        data: {
+          message: 'The user belonging to this token does no longer exist.',
+        },
+      });
+    }
+
+    req.body.user = currentUser;
+    next();
+  } catch (error) {
+    return res.status(400).send({ data: { error } });
+  }
+}
+
+export default auth;
