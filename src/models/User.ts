6ba8d450d5555bfde95868c55c563ae0418e3010
seun jay <seunjay92@gmail.com>
2019-09-17 12:12:33 +0100

    create user model and fix bug with the jwt private key

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+import { createSchema, Type, typedModel } from 'ts-mongoose';
+import jwt from 'jsonwebtoken';
+
+const userSchema = createSchema({
+  name: Type.string(),
+  email: Type.string(),
+  password: Type.string(),
+  isAdmin: Type.optionalBoolean(),
+  ...({} as {
+    getToken: () => string;
+  }),
+});
+
+userSchema.methods.getToken = function() {
+  const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, <any>(
+    process.env.JWT_PRIVATE_KEY
+  ));
+  return token;
+};
+
+export const User = typedModel('User', userSchema);
