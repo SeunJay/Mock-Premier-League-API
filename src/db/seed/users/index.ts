57cc654a790aeb65076b6accdc51f392c3b267bb
seun jay <seunjay92@gmail.com>
2019-09-17 16:22:39 +0100

    create user data for seeding

---
+++ src/db/seed/users/index.ts
@@ -0,0 +1,24 @@
+export = [
+  {
+    email: 'seunjay@gmail.com',
+    name: 'Seun Jay',
+    password: 'test1234',
+    isAdmin: true,
+  },
+  {
+    email: 'kasaprenko@gmail.com',
+    name: 'Alex kasa',
+    password: 'test1234',
+  },
+  {
+    email: 'tboss@gmail.com',
+    name: 'Tobi Coker',
+    password: 'test1234',
+    isAdmin: true,
+  },
+  {
+    email: 'nutty@gmail.com',
+    name: 'Deji Jay',
+    password: 'test1234',
+  },
+];
