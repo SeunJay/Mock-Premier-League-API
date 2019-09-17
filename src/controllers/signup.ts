8adff3f79fba9dd84445bd8619d7f2224d29dec9
seun jay <seunjay92@gmail.com>
2019-09-17 18:24:06 +0100

    create controller for user to sign up

---
+++ src/controllers/signup.ts
@@ -0,0 +1,33 @@
+import { validateUser } from '../validators/validateUser';
+import bcrypt from 'bcrypt';
+import { Request, Response } from 'express';
+import { User } from '../models/User';
+
+//@ts-ignore
+export const signup = async (req: Request, res: Response) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(401).send({ error: error.details[0].message });
+
+  try {
+    const { email } = req.body;
+    let user = await User.findOne({ email });
+    if (user)
+      return res
+        .status(400)
+        .send({ data: { message: 'User already exists!' } });
+
+    user = await new User(req.body);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    const token = user.getToken();
+    res.status(200).send({
+      data: token,
+    });
+  } catch (error) {
+    const { message } = error;
+    return res.status(400).send({
+      data: message,
+    });
+  }
+};
