ab60213b5d16fc16d23b868c9b7fa37913be1ce7
seun jay <seunjay92@gmail.com>
2019-09-18 11:38:49 +0100

    create route for users to signup

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { login } from '../controllers/login';
+import { signup } from '../controllers/signup';
 
 const router = Router();
 
-router.post('/login', login);
+router.post('/login', login).post('/signup', signup);
 
 export default router;
