450a86af536860cb3690442bc43439abdf315148
seun jay <seunjay92@gmail.com>
2019-09-18 11:47:29 +0100

    create route to edit a team

--- src/routes/teams.ts
+++ src/routes/teams.ts
@@ -5,4 +5,7 @@ import { viewTeams, addTeam, editTeam, removeTeam } from '../controllers/team';
 
 const router = Router();
 
-router.get('/', auth, viewTeams).post('/', [auth, admin], addTeam);
+router
+  .get('/', auth, viewTeams)
+  .post('/', [auth, admin], addTeam)
+  .put('/:id', [auth, admin], editTeam);
