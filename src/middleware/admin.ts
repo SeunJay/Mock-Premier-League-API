a101296e85ff0a87a2578921f8b04cab0d3947ce
seun jay <seunjay92@gmail.com>
2019-09-18 18:04:56 +0100

    use bracket notation

--- src/middleware/admin.ts
+++ src/middleware/admin.ts
@@ -2,7 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 
 //@ts-ignore
 function admin(req: Request, res: Response, next: NextFunction) {
-  if (!req.body.isAdmin) {
+  //@ts-ignore
+  if (!req['user'].isAdmin) {
     return res.status(403).send({
       data: { message: 'You do not have permission to perform this action' },
     });
