cd0d05729486da3613e3be83270498853fc45538
seun jay <seunjay92@gmail.com>
2019-09-20 07:21:46 +0100

    write tests for user and admin creating a team

--- src/__tests__/user.ts
+++ src/__tests__/user.ts
@@ -3,21 +3,22 @@ import mongoose from 'mongoose';
 import app from '../app';
 import { User } from '../models/User';
 //import { Team } from '../models/Team';
-import auth from '../middleware/auth';
-import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
+// import auth from '../middleware/auth';
+// import jwt from 'jsonwebtoken';
+// import { Request, Response, NextFunction } from 'express';
 import seed from '../db';
 
 let token: string;
-//let adminToken: string;
+let adminToken: string;
 //let teamA: any;
 //let teamB: any;
 // let fixturesId: string;
 // let fixtureLink: string;
-let session: object;
+// let session: object;
 
 beforeAll(async () => {
   await User.deleteMany({});
+  await seed();
 
   const user = await request(app)
     .post('/api/v1/users/signup')
@@ -28,8 +29,7 @@ beforeAll(async () => {
     });
 
   token = user.body.data;
-  console.log(token);
-  await seed();
+  //console.log(token, user );
 
   // teamA = await Team.findOne({ name: 'Brimingham City' });
   // teamB = await Team.findOne({ name: 'Fulham' });
@@ -153,70 +153,70 @@ describe('Tests for login in a user', () => {
         password: 'test1234',
       })
       .expect(res => {
-        //adminToken = res.body.data.token;
+        adminToken = res.body.data;
         expect(res.body.success).toBe(true);
       });
   });
 });
 
-jest.mock('../middleware/auth');
-const mockedAuth = auth as jest.Mocked<any>;
+// jest.mock('../middleware/auth');
+// const mockedAuth = auth as jest.Mocked<any>;
 
-mockedAuth.mockImplementation(
-  //@ts-ignore
-  async (req: Request, res: Response, next: NextFunction) => {
-    //supertest sees req.session as undefined so had to mock it
+// mockedAuth.mockImplementation(
+//   //@ts-ignore
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     //supertest sees req.session as undefined so had to mock it
 
-    //session store
-    session = {};
-    let token;
-    try {
-      if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-      ) {
-        token = req.headers.authorization.split(' ')[1];
-      }
+//     //session store
+//     session = {};
+//     let token;
+//     try {
+//       if (
+//         req.headers.authorization &&
+//         req.headers.authorization.startsWith('Bearer')
+//       ) {
+//         token = req.headers.authorization.split(' ')[1];
+//       }
 
-      if (!token) {
-        return res.status(401).send({
-          data: {
-            message: 'You are not logged in! Please log in to get access.',
-          },
-        });
-      }
+//       if (!token) {
+//         return res.status(401).send({
+//           data: {
+//             message: 'You are not logged in! Please log in to get access.',
+//           },
+//         });
+//       }
 
-      const decoded: any = jwt.verify(token, <any>process.env.JWT_PRIVATE_KEY);
+//       const decoded: any = jwt.verify(token, <any>process.env.JWT_PRIVATE_KEY);
 
-      //@ts-ignore
-      session[decoded._id] = { token: token };
+//       //@ts-ignore
+//       session[decoded._id] = { token: token };
 
-      if (decoded) {
-        //@ts-ignore
-        if (!session[decoded._id]) {
-          return res.status(401).send({
-            data: { message: 'Session over, Pls login...' },
-          });
-        }
+//       if (decoded) {
+//         //@ts-ignore
+//         if (!session[decoded._id]) {
+//           return res.status(401).send({
+//             data: { message: 'Session over, Pls login...' },
+//           });
+//         }
 
-        //@ts-ignore
-        if (token !== session[decoded._id].token) {
-          return res.status(401).send({
-            data: { message: 'Invalid Token' },
-          });
-        }
+//         //@ts-ignore
+//         if (token !== session[decoded._id].token) {
+//           return res.status(401).send({
+//             data: { message: 'Invalid Token' },
+//           });
+//         }
 
-        //@ts-ignore
-        req['checkUser'] = decoded;
-        next();
-      } else {
-        res.status(401).send({ data: { message: 'user does not exist' } });
-      }
-    } catch (error) {
-      return res.status(400).send({ data: { error } });
-    }
-  },
-);
+//         //@ts-ignore
+//         req['checkUser'] = decoded;
+//         next();
+//       } else {
+//         res.status(401).send({ data: { message: 'user does not exist' } });
+//       }
+//     } catch (error) {
+//       return res.status(400).send({ data: { error } });
+//     }
+//   },
+// );
 
 describe('Tests for team routes', () => {
   it('Users should view teams', () => {
@@ -228,6 +228,53 @@ describe('Tests for team routes', () => {
         expect(res.body.data[0]).toHaveProperty('losses');
         expect(res.body.data[0]).toHaveProperty('name');
         expect(res.body.data[0]).toHaveProperty('email');
+      });
+  });
+
+  it('a regular user should not be able to create a team', () => {
+    console.log(token, 'gere');
+    return request(app)
+      .post(`/api/v1/teams`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'SeunJay FC',
+        email: 'forestgreen@email.com',
+        coach: 'Tob Jay',
+        country: 'England',
+        founded: 2000,
+        stadium_name: 'Well Sparks',
+        wins: 2,
+        losses: 0,
+        goals: 8,
+        stadium_capacity: '60000m',
+      })
+      .expect(res => {
+        // console.log(res)
+        expect(res.body.data.message).toBe(
+          'You do not have permission to perform this action',
+        );
+      });
+  });
+
+  it('an admin user should be able to create a team', () => {
+    console.log(adminToken);
+    return request(app)
+      .post(`/api/v1/teams`)
+      .set('Authorization', `Bearer ${adminToken}`)
+      .send({
+        name: 'SeunJay FC',
+        email: 'forestgreen@email.com',
+        coach: 'Tob Jay',
+        country: 'England',
+        founded: 2000,
+        stadium_name: 'Well Sparks',
+        wins: 2,
+        losses: 0,
+        goals: 8,
+        stadium_capacity: '60000m',
+      })
+      .expect(res => {
+        expect(res.body.success).toBe(true);
       });
   });
 });
